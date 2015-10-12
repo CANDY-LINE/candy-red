70dcd978cd52fc9dc638949466dac3340397aa6c
Daisuke Baba <baba.daisuke@gmail.com>
2015-10-12 10:05:15 +0900

    Adds a module for representing wireless peripherals

---
+++ src/peripherals.js
@@ -0,0 +1,46 @@
+'use strict';
+/*jshint bitwise: false*/
+class BleCastBl {
+  parse(manufacturerData) {
+    let lx = 256 * manufacturerData[5] + manufacturerData[4];
+    return {
+      type: 'temp',
+      unit: 'lux',
+      val: lx,
+      ts: Date.now()
+    };  
+  }
+}
+
+class BleCastTm {
+  parse(manufacturerData) {
+    let tempC = manufacturerData[4] - ((manufacturerData[4] & 0x80) << 1);
+    tempC += ((manufacturerData[5] & 0x80) >> 7) * 0.5;
+    return {
+      type: 'temp',
+      unit: 'C',
+      val: tempC,
+      ts: Date.now()
+    };
+  }
+}
+
+const PERIPHERALS = {
+  BLECAST_BL: new BleCastBl(),
+  BLECAST_TM: new BleCastTm()
+};
+
+class Peripherals {
+  lookup(identifier) {
+    return new Promise((resolve, reject) => {
+      let p = PERIPHERALS[identifier];
+      if (!p) {
+        reject(`Unknown peripheral: ${identifier}`);
+      } else {
+        resolve(p);
+      }
+    });
+  }
+}
+
+export default new Peripherals();
---
+++ test/src/peripherals.test.js
@@ -0,0 +1,60 @@
+import { assert } from 'chai';
+import peripherals from '../../src/peripherals';
+
+describe('Peripherals', () => {
+  describe('#lookup()', () => {
+    it('should return a Promise object', () => {
+      assert.isTrue(peripherals.lookup('BLECAST_BL') instanceof Promise);
+      assert.isTrue(peripherals.lookup('BLECAST_TM') instanceof Promise);
+      assert.isTrue(peripherals.lookup('no-such-id') instanceof Promise);
+    });
+    it('should return a Promise object with reject state when peripherals fails to look up an object', done => {
+      peripherals.lookup('no-such-id').then(() => {
+        assert.fail('Should not reach here!');
+      }).catch(e => {
+        assert.equal('Unknown peripheral: no-such-id', e);
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+  });
+});
+
+describe('BleCastBl', () => {
+  describe('#parse()', () => {
+    it('should parse illuminance values', done => {
+      peripherals.lookup('BLECAST_BL').then(p => {
+        assert.equal(0, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x00, 0x00])).val);
+        assert.equal(0, p.parse(new Buffer([0x99, 0x99, 0xff, 0xff, 0x00, 0x00])).val);
+        assert.equal(1, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x01, 0x00])).val);
+        assert.equal(256, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x00, 0x01])).val);
+        assert.equal(65535, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0xff, 0xff])).val);
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+  });
+});
+
+describe('BleCastTm', () => {
+  describe('#parse()', () => {
+    it('should parse temperature values', done => {
+      peripherals.lookup('BLECAST_TM').then(p => {
+        assert.equal(0, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x00, 0x00])).val);
+        assert.equal(0, p.parse(new Buffer([0x99, 0x99, 0xff, 0xff, 0x00, 0x00])).val);
+        assert.equal(1, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x01, 0x00])).val);
+        assert.equal(0, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x00, 0x01])).val);
+        assert.equal(0.5, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x00, 0x80])).val);
+        assert.equal(-0.5, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0xff, 0xff])).val);
+        assert.equal(-0.5, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0xff, 0x80])).val);
+        assert.equal(-128, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x80, 0x00])).val);
+        assert.equal(127.5, p.parse(new Buffer([0x99, 0x99, 0x00, 0x00, 0x7f, 0x80])).val);
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+  });
+});
