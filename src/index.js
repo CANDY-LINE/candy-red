c74e8edb8c23b590b9e2df2db21dbfea9b26621b
Daisuke Baba <baba.daisuke@gmail.com>
2015-10-12 10:08:07 +0900

    Implements a gateway entry point using a BLE broadcast receiver along with a WebSocket message bus

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
-import http from 'http';
+import * as bus from './bus';
+import * as ble from './ble';
 
-http.createServer((request, response) => {
-  response.end('It Works!! Path Hit: ' + request.url);
-}).listen(8080, () => {
-  console.log('started!');
+let url;
+if (process.argv.length > 1) {
+  url = process.argv[process.argv.length - 1];
+  console.log(`connecting to ${url}`);
+}
+if (!url || url.indexOf('ws') !== 0 || url.indexOf(':') < 0) {
+  console.error('Invalid url');
+  process.exit(1);
+}
+
+bus.start(url).then(() => {
+  return ble.start(bus);
+}).catch(e => {
+  console.error('[ERROR]:', e);
+  if (e instanceof Error) {
+    console.error(e.stack);
+  }
+  process.exit(2);
 });
--- test/src/index.test.js
+++ test/src/index.test.js
@@ -1,10 +1,53 @@
-import assert from 'assert';
+import { assert } from 'chai';
+import { spawn } from 'child_process';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+describe('index.js executable script', () => {
+  it('should not accept missing URL argument', done => {
+    let stdout = '';
+    let stderr = '';
+    let gwd = spawn('node', [`${__dirname}/../../dist/index.js`]);
+    gwd.stdout.on('data', data => {
+      stdout += data.toString();
+    });
+    gwd.stderr.on('data', data => {
+      stderr += data.toString();
+    });
+    gwd.on('exit', code => {
+      assert.equal(1, code);
+      assert.equal('Invalid url', stderr.trim());
+      done();
+    });
+  });
+  it('should not accept a wrong scheme URL', done => {
+    let stdout = '';
+    let stderr = '';
+    let gwd = spawn('node', [`${__dirname}/../../dist/index.js`, 'http://localhost']);
+    gwd.stdout.on('data', data => {
+      stdout += data.toString();
+    });
+    gwd.stderr.on('data', data => {
+      stderr += data.toString();
+    });
+    gwd.on('exit', code => {
+      assert.equal(1, code);
+      assert.equal('Invalid url', stderr.trim());
+      done();
+    });
+  });
+  it('should not accept an invalid URL', done => {
+    let stdout = '';
+    let stderr = '';
+    let gwd = spawn('node', [`${__dirname}/../../dist/index.js`, 'http_//_localhost']);
+    gwd.stdout.on('data', data => {
+      stdout += data.toString();
+    });
+    gwd.stderr.on('data', data => {
+      stderr += data.toString();
+    });
+    gwd.on('exit', code => {
+      assert.equal(1, code);
+      assert.equal('Invalid url', stderr.trim());
+      done();
     });
   });
 });
