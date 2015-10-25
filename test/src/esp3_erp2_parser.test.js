f8adf1bdf63c287a88259d977a3e0870b68228b5
Daisuke Baba <baba.daisuke@gmail.com>
2015-10-25 17:02:20 +0900

    Add EnOcean Serial Protocol 3 (ESP3) format parser and EnOcean Radio Protocol 2 (ERP2) parser
    This module offers the ERP2 parser and the ESP3 parser dedicated to ERP2 packet type (other packet types are unsupported).
    These classes are introduced because the existing enocnean npm doesn't support the RADIO_ERP2 packet type.
    The ERP2 parser doesn't fully support ERP2 specification, i.e. Extended Header(4.5) and Extended Telegram type(4.5) are excluded.

---
+++ test/src/esp3_erp2_parser.test.js
@@ -0,0 +1,59 @@
+import { assert } from 'chai';
+import { ERP2Parser, ESP3RadioERP2Parser } from '../../src/esp3_erp2_parser';
+
+describe('ERP2Parser', () => {
+  let parser = new ERP2Parser();
+  describe('#parse()', () => {
+    it('should return a valid context from a raw data passed from node-enocean', done => {
+      parser.parse('20002bcaa98861').then(ctx => {
+        assert.equal('002bcaa9', ctx.originatorId);
+        assert.equal('', ctx.destinationId);
+        assert.equal('RPS', ctx.telegramType);
+        assert.equal(0xf6, ctx.rorg);
+        assert.equal('88', new Buffer(ctx.dataDl).toString('hex'));
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+    it('should return a valid context from another raw data passed from node-enocean', done => {
+      // 550007020a0a20002bcaa900d001328b
+      parser.parse('20002bcaa900d0').then(ctx => {
+        assert.equal('002bcaa9', ctx.originatorId);
+        assert.equal('', ctx.destinationId);
+        assert.equal('RPS', ctx.telegramType);
+        assert.equal(0xf6, ctx.rorg);
+        assert.equal('00', new Buffer(ctx.dataDl).toString('hex'));
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+  });
+});
+
+describe('ESP3RadioERP2Parser', () => {
+  let parser = new ESP3RadioERP2Parser();
+  describe('#parse()', () => {
+    it('should return a valid context from a raw data passed from node-enocean', done => {
+      parser.parse('550007020a0a20002bcaa9886101328b').then(ctx => {
+        assert.equal(0x01, ctx.subTelNum);
+        assert.equal(0x32, ctx.dBm);
+        assert.equal('20002bcaa98861', new Buffer(ctx.payload).toString('hex'));
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+    it('should return a valid context from another raw data passed from node-enocean', done => {
+      parser.parse('550007020a0a20002bcaa900d001328b').then(ctx => {
+        assert.equal(0x01, ctx.subTelNum);
+        assert.equal(0x32, ctx.dBm);
+        assert.equal('20002bcaa900d0', new Buffer(ctx.payload).toString('hex'));
+        done();
+      }).catch(e => {
+        done(e); // for showing assertion errors
+      });
+    });
+  });
+});
