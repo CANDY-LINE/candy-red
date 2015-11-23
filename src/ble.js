32748c01b069e650cdb1d9c41b3a1238a7e07904
Daisuke Baba <baba.daisuke@gmail.com>
2015-11-23 15:31:29 +0900

    Add Node-RED node support
    The module is expected to be used by local Node-RED nodes for BLE.
    It is a common shared library so that multiple BLE nodes are able to share the code.

--- src/ble.js
+++ src/ble.js
@@ -1,63 +1,135 @@
 'use strict';
 
 import noble from 'noble';
-import peripherals from './ble_peripherals';
 import Promise from 'es6-promises';
 
-export function start(bus) {
-  if (!bus) {
-    throw new Error('bus is required!');
+let peripheralsIn = {};
+let isScanning = false;
+
+/**
+ * Associate the given in-Node object with the BLE module.
+ * @param n the in-Node object to be registered as a BLE node
+ * @param categoryName the category name
+ * @param address the ble address delimited by '-'
+ * @param uuid the ble identifier (optional)
+ * @param parse the parse function
+ * @param RED the initialized RED object
+ * @return void (sync)
+ */
+export function registerIn(n, categoryName, address, uuid, parse, RED) {
+  if (!n || !categoryName || !address || !parse) {
+    throw new Error('Invalid node!');
+  }
+  if (!RED) {
+    throw new Error('RED is required!!');
+  }
+  let category = peripheralsIn[categoryName];
+  if (!category) {
+    category = {};
+    peripheralsIn[categoryName] = category;
+  }
+  category[address] = {
+    node: n,
+    parse: parse
+  };
+  if (uuid) {
+    category[uuid] = category[address];
+    RED.log.info(`[BLE] category=[${categoryName}], address=[${address}, uuid=[${uuid}]] has been registered.`);
+  } else {
+    RED.log.info(`[BLE] category=[${categoryName}], address=[${address}] has been registered.`);
+  }
+}
+
+/**
+ * Start the BLE module.
+ * @param RED the initialized RED object
+ * @return Promise
+ */
+export function start(RED) {
+  if (!RED) {
+    throw new Error('RED is required!');
   }
   return new Promise((resolve, reject) => {
+    if (isScanning) {
+      resolve();
+      return;
+    }
     noble.on('stateChange', state => {
       if (state === 'poweredOn') {
-        console.log('Starting Scanning...');
+        RED.log.info('[BLE] Start Scanning...');
         noble.startScanning([], true);
+        isScanning = true;
         resolve();
       } else {
         noble.stopScanning();
+        isScanning = false;
         reject();
       }
     });
     if (noble.state === 'poweredOn') {
-      console.log('Starting Scanning...');
+      RED.log.info('[BLE] Start Scanning...');
       noble.startScanning([], true);
+      isScanning = true;
       resolve();
     }
   }).then(() => {
     return new Promise((resolve, reject) => {
       noble.on('discover', peripheral => {
         let adv = peripheral.advertisement;
-        peripherals.lookup(adv.localName).then(p => {
-          let data = p.parse(adv.manufacturerData);
-          let payload = {
-            data: data,
-            tstamp: Date.now(),
-            rssi: peripheral.rssi,
-            id: peripheral.address
-          };
-          if (payload.id === 'unknown') {
-            // OSX workaround
-            payload.id = peripheral.id;
+        // Remove a NULL terminator
+        let categoryName = adv.localName.replace(new RegExp('\0', 'g'), '');
+        // look up a category by the category name
+        let category = peripheralsIn[categoryName];
+        if (!category) {
+          RED.log.warn(`[BLE] Unknown peripheral: category=[${categoryName}], peripheral.address=[${peripheral.address}], peripheral.uuid=[${peripheral.uuid}]`);
+          return;
+        }
+        // check if the peripheral.address matches
+        let address = peripheral.address;
+        let uuid = null;
+        let bleNode = null;
+        if (address === 'unknown') {
+          uuid = peripheral.uuid;
+          bleNode = category[uuid];
+          if (!bleNode) {
+            RED.log.warn(`[BLE] Unknown node: category=[${categoryName}], uuid=[${uuid}]`);
+            return;
           }
-          return bus.send(payload);
-        }).catch(e => {
-          if (e instanceof Error) {
-            reject(e);
-          } else {
-            console.log(e, peripheral);
+        }
+        if (!uuid) {
+          bleNode = category[address];
+          if (!bleNode) {
+            RED.log.warn(`[BLE] Unknown node: category=[${categoryName}], address=[${address}]`);
+            return;
           }
-        });
+        }
+        // send the ble node a payload if the address exists
+        let node = bleNode.node;
+        let data = bleNode.parse(adv.manufacturerData);
+        let payload = {
+          data: data,
+          tstamp: Date.now(),
+          rssi: peripheral.rssi,
+          address: address
+        };
+        if (uuid) {
+          payload.uuid = uuid;
+        }
+        node.send({'payload': payload});
       });
       resolve();
+      RED.log.info('[BLE] Set up done.');
     });
   });
 }
 
-export function stop() {
-  return new Promise(resolve => {
-    noble.stopScanning();
-    console.log('Scanning stopped.');
-    resolve();
-  });  
+/**
+ * Stop the BLE module immediately.
+ * @param RED the initialized RED object
+ * @return void (sync)
+ */
+export function stop(RED) {
+  noble.stopScanning();
+  isScanning = false;
+  RED.log.info('[BLE] Stop scanning.');
 }
