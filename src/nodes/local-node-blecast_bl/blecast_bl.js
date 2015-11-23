eb5283da527d0f6c38e68875d21dc9a4abb42964
Daisuke Baba <baba.daisuke@gmail.com>
2015-11-23 15:18:47 +0900

    Add a new local node for BLE illuminance sensor tag

---
+++ src/nodes/local-node-blecast_bl/blecast_bl.js
@@ -0,0 +1,34 @@
+'use strict';
+/*
+ * BLECAST_BL (BLE with Illuminance Sensor) node
+ */
+
+import noble from 'noble';
+import Promise from 'es6-promises';
+import * as blecastBl from './lib/blecast_bl';
+
+const CATEGORY = 'BLECAST_BL';
+
+export default function(RED) {
+  let ble = RED.settings.ble;
+  ble.start(RED).then(enabled => {
+    function BlecastBlNode(n) {
+      RED.nodes.createNode(this, n);
+      this.address = n.address;
+      this.uuid = n.uuid;
+    }
+    RED.nodes.registerType(CATEGORY, BlecastBlNode);
+
+    function BlecastBlInNode(n) {
+      RED.nodes.createNode(this, n);
+      this.blecastBlNodeId = n.blecastBl;
+      this.blecastBlNode = RED.nodes.getNode(this.blecastBlNodeId);
+      ble.registerIn(this, CATEGORY, this.blecastBlNode.address, this.blecastBlNode.uuid, blecastBl.parse, RED);
+      this.name = n.name;
+    }
+    RED.nodes.registerType(`${CATEGORY} in`, BlecastBlInNode);
+
+  }).catch(e => {
+    RED.log.error(RED._('blecast_bl.errors.unknown', { error: e }));
+  });
+}
