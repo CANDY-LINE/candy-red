00aa08541c083b38f37340193d340b362083712d
Daisuke Baba <baba.daisuke@gmail.com>
2015-11-25 21:56:44 +0900

    Merge the same vendor nodes into a single package
    This change makes it possible to add a new node without adding a new npm package, which will be useful when the package is public in the future.

---
+++ src/nodes/local-node-asakusa_giken/asakusa_giken.js
@@ -0,0 +1,52 @@
+'use strict';
+/*
+ * BLECAST_BL (BLE with Illuminance Sensor) node
+ * BLECAST_TM (BLE with Temeprature Sensor) node
+ */
+
+import * as blecastBl from './lib/blecast_bl';
+import * as blecastTm from './lib/blecast_tm';
+
+export default function(RED) {
+  let ble = RED.settings.ble;
+  ble.start(RED).then(() => {
+    function BlecastBlNode(n) {
+      RED.nodes.createNode(this, n);
+      this.address = n.address;
+      this.uuid = n.uuid;
+    }
+    RED.nodes.registerType('BLECAST_BL', BlecastBlNode);
+
+    function BlecastBlInNode(n) {
+      RED.nodes.createNode(this, n);
+      this.useString = n.useString;
+      this.blecastBlNodeId = n.blecastBl;
+      this.blecastBlNode = RED.nodes.getNode(this.blecastBlNodeId);
+      ble.registerIn(this, CATEGORY, this.blecastBlNode.address, this.blecastBlNode.uuid,
+        blecastBl.parse, this.useString, RED);
+      this.name = n.name;
+    }
+    RED.nodes.registerType('BLECAST_BL in', BlecastBlInNode);
+
+    function BlecastTmNode(n) {
+      RED.nodes.createNode(this, n);
+      this.address = n.address;
+      this.uuid = n.uuid;
+    }
+    RED.nodes.registerType('BLECAST_TM', BlecastTmNode);
+
+    function BlecastTmInNode(n) {
+      RED.nodes.createNode(this, n);
+      this.useString = n.useString;
+      this.blecastTmNodeId = n.blecastTm;
+      this.blecastTmNode = RED.nodes.getNode(this.blecastTmNodeId);
+      ble.registerIn(this, CATEGORY, this.blecastTmNode.address, this.blecastTmNode.uuid,
+        blecastTm.parse, this.useString, RED);
+      this.name = n.name;
+    }
+    RED.nodes.registerType('BLECAST_TM in', BlecastTmInNode);
+
+  }).catch(e => {
+    RED.log.error(RED._('blecast_bl.errors.unknown', { error: e }));
+  });
+}
