fdea38011e3c170edd75f8977964d8410e86c73a
Daisuke Baba <baba.daisuke@gmail.com>
2016-02-09 10:55:23 +0900

    Allow install.sh to run on the development environment as well as production environment

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ function info {
 }
 
 function download_and_npm_install {
-  info "Performing npm install ${SERVICE_NAME}@${VERSION}..."
+  info "Performing npm install ${SERVICE_NAME}..."
   npm install -g --unsafe-perm ${SERVICE_NAME}
 }
 
@@ -48,9 +48,11 @@ function cpf {
 }
 
 function assert_root {
-  if [[ $EUID -ne 0 ]]; then
-     err "This script must be run as root"
-     exit 1
+  if [ ! -d "${PPOJECT_ROOT}/src" ]; then
+    if [[ $EUID -ne 0 ]]; then
+       err "This script must be run as root"
+       exit 1
+    fi
   fi
 }
 
@@ -61,11 +63,9 @@ function assert_node_npm {
   fi
 }
 
-function test_system_service_arg {
-  if [ "$1" == "" ]; then
+function test_system_service {
+  if [ ! -d "${PPOJECT_ROOT}/src" ]; then
     _try_systemd
-  else
-    SYSTEM_SERVICE_TYPE="$1"
   fi
 }
 
@@ -88,60 +88,43 @@ function cd_module_root {
   else
     REALPATH=`readlink -f -- "$0"`
   fi
-  ROOT=`dirname ${REALPATH}`
-  cd ${ROOT}
+  PPOJECT_ROOT=`dirname ${REALPATH}`
+  cd ${PPOJECT_ROOT}
 }
 
 function resolve_version {
-  if [ -f "${ROOT}/package.json" ]; then
+  if [ "$1" == "update" ]; then
+    unset VERSON
+  elif [ -f "${PPOJECT_ROOT}/package.json" ]; then
     # https://gist.github.com/DarrenN/8c6a5b969481725a4413
-    VERSION=$(cat ${ROOT}/package.json \
+    VERSION=$(cat ${PPOJECT_ROOT}/package.json \
       | grep version \
       | head -1 \
       | awk -F: '{ print $2 }' \
       | sed 's/[",]//g' \
       | tr -d '[[:space:]]')
-    LATEST=$(curl -L https://github.com/dbaba/candy-red/raw/master/package.json \
-      | grep version \
-      | head -1 \
-      | awk -F: '{ print $2 }' \
-      | sed 's/[",]//g' \
-      | tr -d '[[:space:]]')
-    if [ -z "${VERSION}" ]; then
-      if [ -z "${LATEST}" ]; then
-        err "Failed to resolve the latest version"
-        exit 4
-      fi
-      info "Installing the latest version of CANDY RED..."
-    elif [ -z "${LATEST}" ]; then
-      info "Re-installing CANDY RED..."
-    elif [ "${VERSION}" != "${LATEST}" ]; then
-      info "Upgrading CANDY RED: ${VERSION} => ${LATEST}..."
-      unset VERSION
-    fi
   fi
   if [ -z "${VERSION}" ]; then
     VERSION="master"
     download_and_npm_install
-    $(npm root -g)/candy-red/install.sh
     exit $?
   fi
 }
 
 function npm_local_install {
-  if [ -d "./dist" ]; then
+  if [ -d "${PROJECT_ROOT}/dist" ]; then
     cp -r ./dist/nodes/local-node-* node_modules/
   fi
 }
 
 function system_service_install {
-  SERVICES="${ROOT}/services"
+  SERVICES="${PPOJECT_ROOT}/services"
   START_SH="${SERVICES}/start_${SYSTEM_SERVICE_TYPE}.sh"
 
   rm -f ${SERVICES}/start_*
   cpf ${SERVICES}/_start.sh ${START_SH}
   sed -i -e "s/%SERVICE_NAME%/${SERVICE_NAME//\//\\/}/g" ${START_SH}
-  sed -i -e "s/%SERVICE_HOME%/${ROOT//\//\\/}/g" ${START_SH}
+  sed -i -e "s/%SERVICE_HOME%/${PPOJECT_ROOT//\//\\/}/g" ${START_SH}
 
   cp -f ${SERVICES}/base_environment.txt ${SERVICES}/environment
   sed -i -e "s/%HCIDEVICE%/${HCIDEVICE//\//\\/}/g" ${SERVICES}/environment
@@ -159,7 +142,7 @@ function _install_systemd {
   LIB_SYSTEMD="${LIB_SYSTEMD}/lib/systemd"
 
   cpf ${LOCAL_SYSTEMD}/${SERVICE_NAME}.service.txt ${LOCAL_SYSTEMD}/${SERVICE_NAME}.service
-  sed -i -e "s/%SERVICE_HOME%/${ROOT//\//\\/}/g" ${LOCAL_SYSTEMD}/${SERVICE_NAME}.service
+  sed -i -e "s/%SERVICE_HOME%/${PPOJECT_ROOT//\//\\/}/g" ${LOCAL_SYSTEMD}/${SERVICE_NAME}.service
   sed -i -e "s/%VERSION%/${VERSION//\//\\/}/g" ${LOCAL_SYSTEMD}/${SERVICE_NAME}.service
 
   cpf ${SERVICES}/environment ${LOCAL_SYSTEMD}/environment
@@ -171,14 +154,16 @@ function _install_systemd {
   info "${SERVICE_NAME} service has been installed."
 }
 
-setup
-test_system_service_arg
 cd_module_root
+setup $1
 resolve_version
-if [ "${SYSTEM_SERVICE_TYPE}" != "" ]; then
-  ${ROOT}/uninstall.sh system_service
+test_system_service
+if [ -n "${SYSTEM_SERVICE_TYPE}" ]; then
+  ${PPOJECT_ROOT}/uninstall.sh
   npm_local_install
   system_service_install
+elif [ -d "${PPOJECT_ROOT}/src" ]; then
+  info "Skip to setup a SystemD service"
 else
   info "Won't install a SystemD service as it isn't supported on the system"
   npm_local_install
