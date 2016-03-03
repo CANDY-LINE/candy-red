02d187633c3a1559aa1a917f9e4a126be8ec48d7
Daisuke Baba <baba.daisuke@gmail.com>
2016-03-03 13:33:01 +0900

    Show working status on receiving msg from the input port

--- src/nodes/local-node-device-stats/device-stats.es6.js
+++ src/nodes/local-node-device-stats/device-stats.es6.js
@@ -16,14 +16,20 @@ export default function(RED) {
       this.collector = new StatsCollector(this);
 
       this.on('input', msg => {
+        clearTimeout(this.timeout);
+        this.status({ fill: 'red', shape: 'dot', text: 'device-stats.status.heartbeat' });
         let opts = msg ? msg.payload : null;
         this.collector.collect(opts).then(stats => {
           if (this.useString || opts && opts.useString) {
             stats = JSON.stringify(stats);
           }
           this.send({ payload: stats });
+          this.timeout = setTimeout(() => {
+            this.status({});
+          }, 500);
         }).catch(err => {
           RED.log.warn(RED._('device-stats.errors.unknown', { error: err }));
+          this.status({});
         });
       });
     }
