c623124d0c34911d4502b51b1d6013e6513675e3
Daisuke Baba <baba.daisuke@gmail.com>
2017-11-16 11:30:35 +0900

    Compute --max-old-space-size value when the option is missing

--- services/_start.sh
+++ services/_start.sh
@@ -14,6 +14,16 @@ if [ "${RET}" == "0" ]; then
   hciconfig ${HCIDEVICE} reset
 fi
 
+if [[ ${NODE_OPTS} != *--max-old-space-size=* ]]; then
+  if [ -n "`which free`" ]; then
+    MEM=`free -m | grep "Mem:" | awk '{print $2}'`
+    MAX_OLD_SPACE_SIZE=${MAX_OLD_SPACE_SIZE:-`expr ${MEM} / 3`}
+  fi
+  MAX_OLD_SPACE_SIZE=${MAX_OLD_SPACE_SIZE:-256}
+  NODE_OPTS="${NODE_OPTS} --max-old-space-size=${MAX_OLD_SPACE_SIZE}"
+fi
+logger -s "node options => ${NODE_OPTS}"
+
 mkdir -p /opt/candy-red
 
 # Disable i18n resources other than en-US for now (will be enabled in the future release)
