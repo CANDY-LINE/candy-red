f2fc070de3fc0d7361b6a065f52ad5d17e875ddf
Daisuke Baba <baba.daisuke@gmail.com>
2019-04-05 15:53:19 +0900

    Fix the wrong truncate length

--- uninstall.sh
+++ uninstall.sh
@@ -56,7 +56,7 @@ function _lookup_system_service_type {
     err "The service ${SERVICE_NAME} isn't installed yet."
   else
     START_SH=$(basename ${START_SH})
-    SYSTEM_SERVICE_TYPE=${START_SH:6:`echo ${START_SH} | wc -c`-9}
+    SYSTEM_SERVICE_TYPE=${START_SH:6:`echo ${START_SH} | wc -c`-10}
   fi
 }
 
